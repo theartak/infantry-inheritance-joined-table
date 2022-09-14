a2626b00af2b0617d03ff58f5dba3749ba454507
theartak <kartachok@gmail.com>
2022-09-14 15:57:34 +0400

    abstract

--- src/main/java/com/jambit/artinheritancejoinedtable/model/Infantry.java
+++ src/main/java/com/jambit/artinheritancejoinedtable/model/Infantry.java
@@ -11,7 +11,7 @@ import javax.persistence.InheritanceType;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public class Infantry {
+public abstract class Infantry {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
